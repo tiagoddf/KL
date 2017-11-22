9e5cfebcd0b945486157ecef435a03ae76f8790a
tiagoddf <33496838+tiagoddf@users.noreply.github.com>
2017-11-22 17:46:00 -0200

    1.0

---
+++ src/kl/KL.java
@@ -0,0 +1,33 @@
+package kl;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class KL implements NativeKeyListener {
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            Logger.getLogger(KL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent e) {
+       System.out.println("Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent nke) {
+       
+    }
+}
